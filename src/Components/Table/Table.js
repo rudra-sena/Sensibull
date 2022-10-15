8eab7afaf54e898430b3d9fb27fa490ce855feba
rudra-sena <suhugilly22799@gmail.com>
2022-10-15 05:35:53 +0530

    Changed to bootstrap-table

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,63 +1,38 @@
-import {useMemo} from 'react';
-import {useTable} from 'react-table';
+import {useEffect,useMemo} from 'react';
+import { useNavigate } from "react-router-dom";
+import BootstrapTable from 'react-bootstrap-table-next';
+import filterFactory from 'react-bootstrap-table2-filter';
 import './Table.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.css'
 
-const Table = ({tableData,tableColumns}) => {
 
-    const columns = useMemo(()=>tableColumns,[])
-    const data = useMemo(()=>tableData,[])
+const Table = ({tableData,tableColumns,tableType}) => {
+
+    const navigate = useNavigate();
+    const columns = useMemo(()=>tableColumns,[]);
+    const data = useMemo(()=>tableData,[]);
+
+    useEffect(()=>{
+        if(tableType === 'stocksTable'){
+            const rows=document.querySelectorAll('tbody tr');
+        rows.forEach((row)=>{
+            row.addEventListener('click',()=>{
+                const id=row.cells[0].innerText;
+                navigate(`/quotes/${id}`)
+            })
+        })}
+    },[tableData])
     
-    const tableObject = useTable({
-        columns,
-        data
-    })
-
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow
-    } = tableObject
-
     return ( 
         <>
-            <table {...getTableProps()}>
-                <thead>
-                {
-                headerGroups.map(headerGroup => (
-                    
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                    {
-                    headerGroup.headers.map(column => (
-                        <th {...column.getHeaderProps()}>
-                        {
-                        column.render('Header')}
-                        </th>
-                    ))}
-                    </tr>
-                ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                {
-                rows.map(row => {
-                    prepareRow(row)
-                    return (
-                    <tr {...row.getRowProps()}>
-                        {
-                        row.cells.map(cell => {
-                        return (
-                            <td {...cell.getCellProps()}>
-                            {
-                            cell.render('Cell')}
-                            </td>
-                        )
-                        })}
-                    </tr>
-                    )
-                })}
-                </tbody>
-            </table>
+          <BootstrapTable
+           bootstrap4
+            keyField={tableColumns[0]}
+            columns={columns}
+            data={data}
+            filter={filterFactory()}
+            />
         </>
      );
 }
