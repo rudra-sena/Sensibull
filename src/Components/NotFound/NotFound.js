355ee65c08d04a6ba40e23209e6cf05ea2e3d932
rudra-sena <suhugilly22799@gmail.com>
2022-10-16 00:52:19 +0530

    Logo and Animations

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,7 +1,11 @@
 const NotFound = () => {
     return ( 
         <>
-            <h1>404 Page</h1>
+            <div className="error-container" style={{backgroundColor: '#ececec'}}>
+                <h1>404 Error</h1>
+                <h6>Ooops...The resource you were looking for was not found :&#40;</h6>
+            </div>
+
         </>
      );
 }
