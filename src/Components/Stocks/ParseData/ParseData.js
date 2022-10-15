ffee87239feb15ad87c379d2aa5ec2e222c1984d
rudra-sena <suhugilly22799@gmail.com>
2022-10-15 05:39:13 +0530

    Search Filter for stocks

--- src/Components/Stocks/ParseData/ParseData.js
+++ src/Components/Stocks/ParseData/ParseData.js
@@ -1,5 +1,7 @@
+import {useEffect} from 'react';
 import { readString } from 'react-papaparse';
 import Table from '../../Table/Table'
+import {textFilter,Comparator} from 'react-bootstrap-table2-filter';
 
 const ParseData = ({CSVData}) => {
     
@@ -8,18 +10,47 @@ const ParseData = ({CSVData}) => {
         skipEmptyLines:true
     })) 
     const stocksData=parsedData.data;
-
     const columns= Object.keys(stocksData[0]).filter((key) => key!=='Validtill');
+
     const columnHeaders=columns.map((column) =>{
-        return {
-            Header: column,
-            accessor: column
+        if(column==='Symbol' || column==='Name'){
+            return {
+                dataField: column,
+                text: column,
+                headerFormatter: columnFormatter,
+                filter: textFilter({
+                    delay: 1000,
+                    placeholder: 'Search...',
+                })
+            }
+        }
+        else{
+            return {
+                dataField: column,
+                text: column
+            }
         }
     })
 
+    function columnFormatter(column, colIndex, { filterElement }) {    
+        return (
+          <div style={ { display: 'flex', flexDirection: 'column' } }>
+            { column.text }
+            { filterElement }   
+          </div>
+        );
+      }
+
+    useEffect(()=>{
+        const labels=document.querySelectorAll('span.sr-only');
+        labels.forEach((label) => {
+            label.innerText = "";
+        },[])
+    })
+    
     return ( 
         <>
-            <Table tableData={stocksData} tableColumns={columnHeaders}/>
+            <Table tableData={stocksData} tableColumns={columnHeaders} tableType="stocksTable"/>
         </>
      );
 }
